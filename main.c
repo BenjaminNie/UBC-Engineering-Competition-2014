e11b8b795d8b52e38b7cba555a5cfa70da3a35dc
BenjaminNie <benjamin.nie.ubc@gmail.com>
2014-11-07 02:31:38 -0800

    adding test plan and modified main.c

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
-testing 123
+task main()
+{
+	  int state = 0;
+
+	  while(1) {
+			switch (state) {
+				case(0):
+					break;
+
+				case(1):
+					break;
+	  }
+}
+
+
+/*
+Plan:
+Have state machines based on L, C, R sensor values.  Place sensors far enough from eachother such that only two sensors can be triggered at a time.
