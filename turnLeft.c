325dd0e9d8a1ba0735e96eff5e69994fcc074bdf
grobyn <robynroopgill@gmail.com>
2014-11-08 13:43:30 -0800

    turnleft and turn right updated at 1:43

--- turnLeft.c
+++ turnLeft.c
@@ -1,5 +1,6 @@
 // Code for Turning left //
-
+#pragma config(Sensor, dgtl1,  rightEncoder,        sensorQuadEncoder)
+#pragma config(Sensor, dgtl3,  leftEncoder,         sensorQuadEncoder)
 #pragma config(Motor,  motor1,          leftMotor,     tmotorVexIQ, openLoop, encoder)
 #pragma config(Motor,  motor6,          rightMotor,    tmotorVexIQ, openLoop, reversed, encoder)
 
@@ -15,14 +16,14 @@ turnLeft(turnSpeed);
 void turnLeft()
 {
 
-	int turnDistance = 3;
+	int turnDistance = 5;
 
 	//resetMotorEncoder(leftMotor);		//Reset the current position in the motor encoder to zero.
 	//resetMotorEncoder(rightMotor);
 
 	setMotorSpeed(leftMotor, 0);
 	setMotorSpeed(rightMotor, 0);
-	int rightEncoder = getMotorEncoder(rightMotor);
+	int rightEncoder = SensorValue(rightEncoder);
 
 
 	wait1Msec(50); //Small delay to allow motors time to start moving before checking
@@ -30,10 +31,10 @@ void turnLeft()
 	// moveMotorTarget(leftMotor, 5000, 75);  //Set motor to run 5000 counts at power level 75.
 	// moveMotorTarget(rightMotor, 5000, 75);
 
-  while (getMotorEncoder(rightMotor) > (turnDistance+rightEncoder))
+  while (SensorValue(rightEncoder) > (turnDistance+rightEncoder))
 	{
 		setMotorSpeed(leftMotor, 50);
-		displayTextLine(1, "Left Motor: %d", getMotorEncoder(rightMotor));
+		displayTextLine(1, "Left Motor: %d", SensorValue(leftEncoder));
 		sleep(50);
 	}
 
--- turnRight.c
+++ turnRight.c
@@ -1,5 +1,6 @@
 // Code for Turning left //
-
+#pragma config(Sensor, dgtl1,  rightEncoder,        sensorQuadEncoder)
+#pragma config(Sensor, dgtl3,  leftEncoder,         sensorQuadEncoder)
 #pragma config(Motor,  motor1,          leftMotor,     tmotorVexIQ, openLoop, encoder)
 #pragma config(Motor,  motor6,          rightMotor,    tmotorVexIQ, openLoop, reversed, encoder)
 
@@ -13,8 +14,8 @@ turnRight(turnSpeed);
 
 void turnRight(turnSpeed)
 {
-	int turnDistance = 3;
-	int leftEncoder = getMotorEncoder(leftMotor);
+	int turnDistance = 5;
+	int leftEncoder = SensorValue(leftEncoder);
 
 	//resetMotorEncoder(leftMotor);		//Reset the current position in the motor encoder to zero.
 	//resetMotorEncoder(rightMotor);
@@ -22,10 +23,10 @@ void turnRight(turnSpeed)
 	setMotorSpeed(leftMotor, 0);
 	setMotorSpeed(rightMotor, 0);
 
-  while (getMotorEncoder(leftMotor) > (turnDistance+leftEncoder))
+  while (SensorValue(leftEncoder) > (turnDistance+leftEncoder))
 	{
 		setMotorSpeed(leftMotor, turnSpeed);
-		displayTextLine(1, "Left Motor: %d", getMotorEncoder(leftMotor));
+		displayTextLine(1, "Left Motor: %d", SensorValue(leftEncoder));
 		sleep(50);
 	}
 
